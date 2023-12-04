672ae5ee327fa194d08b57d89944cbce4e45ae8f
João Marcos <jmarcossferreira@hotmail.com>
2023-12-04 18:59:14 -0300

    fix: revert changes on home page

--- src/app/ui/pages/home/home.component.ts
+++ src/app/ui/pages/home/home.component.ts
@@ -1,17 +1,23 @@
-import { Component, OnInit, computed, inject } from '@angular/core';
+import { Component, OnInit, inject } from '@angular/core';
 import { AuthenticationGateway } from '../../../remote/gateway/authentication.gateway';
 
 @Component({
   standalone: true,
   template: `<section class="h-page flex flex-col gap-8 items-center justify-center">
     <h1 class="text-lg font-bold">Welcome to Angular 17!!</h1>
-    @if(isAuthenticated()){
-    <p>Welcome, {{ userName() }}!</p>
+    @if(isAuthenticated){
+    <p>Welcome, {{ userName }}!</p>
     }
   </section>`,
 })
 export class HomePageComponent {
   private _authGateway = inject(AuthenticationGateway);
-  isAuthenticated = computed(() => this._authGateway.isAuthenticated);
-  userName = computed(() => this._authGateway.authenticatedUser?.name ?? '');
+
+  get isAuthenticated(): boolean {
+    return this._authGateway.isAuthenticated;
+  }
+
+  get userName(): string {
+    return this._authGateway.authenticatedUser?.name ?? '';
+  }
 }
